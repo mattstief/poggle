36c3848c4050c0e5c6f483e5551fab201b0e1355
Matt Stieferman <mstieferman@gmail.com>
2022-06-03 17:11:31 -0500

    static typing resizer script

--- scripts/resizer.gd
+++ scripts/resizer.gd
@@ -4,15 +4,16 @@ var screen_size : Vector2 = Vector2()
 var follow_shot : bool	  = true
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = OS.get_screen_size()
+func _ready() -> void:
+	var size:Vector2 = OS.get_screen_size()
+	self.set("screen_size", size)
 
 func _process(_delta) -> void:
-	var curr_size = OS.get_screen_size()
+	var curr_size:			Vector2 = OS.get_screen_size()
 	var screen_size_changed:bool = (curr_size != screen_size)
 	if screen_size_changed:
-		screen_size = curr_size
-		OS.set_window_size(screen_size)
+		self.set("screen_size", curr_size)
+		OS.set_window_size(curr_size)
 
-func _projectile_shot():
+func _projectile_shot() -> void:
 	pass
