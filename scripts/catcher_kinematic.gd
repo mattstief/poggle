894a3d9ec76ee74bb4baf7c9f7a9302c275c7384
Matt Stieferman <mstieferman@gmail.com>
2022-06-02 22:49:14 -0500

    static typing in catcher_kinematic script vars and funcs

--- scripts/catcher_kinematic.gd
+++ scripts/catcher_kinematic.gd
@@ -1,36 +1,44 @@
 extends KinematicBody2D
-export(float) var period = 10.0
-export(float) var amplitude
-var tween
-var tween_enum = 0
+export(float) var period:	float = 10.0
+export(float) var amplitude:float
+var tween:		Tween
+var tween_enum:	int = 0
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-func collide():
+func collide() -> void:
 	pass
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if !self.get("amplitude"):
-		amplitude = abs(self.get("global_position").x)
-	var new_tween = Tween.new()
-	self.add_child(new_tween)
-	self.set("tween", new_tween)
-	var tween_exists:bool = is_instance_valid(self.get("tween"))
-	if tween_exists:
-		tween_loop()
+	var amplitude_set:bool = self.get("amplitude")
+	if not amplitude_set:
+		guess_starting_amplitude()
+	initialize_tween()
 
-func tween_loop():
-	while(true):
-		update_tween()
-		tween.start()
-		yield(tween, "tween_completed")
+func guess_starting_amplitude() -> void:
+	var start_pos:			Vector2	= self.get("global_position")
+	var initial_x_offset:	float 	= start_pos.x
+	var amp:			 	float 	= abs(initial_x_offset)
+	self.set("amplitude", amp)
+
+func initialize_tween():
+	var new_tween:		Tween = Tween.new()
+	var tween_exists:	bool  = is_instance_valid(new_tween)
+	if tween_exists:
+		self.add_child(new_tween)
+		self.set("tween", new_tween)
+		tween_loop(new_tween)
+
+func tween_loop(twn:Tween) -> void:
+	while(true):	#infinite loop
+		var update:bool = update_tween()
+		if update:
+			var start:bool 	= twn.start()
+			if start:
+				yield(twn, "tween_completed")
 
 func tween_property(start, end, obj, property_str, tween_obj,
-duration = period/4.0, trans_type = Tween.TRANS_LINEAR, ease_type = Tween.EASE_IN_OUT):
+duration = period/4.0, trans_type = Tween.TRANS_LINEAR, ease_type = Tween.EASE_IN_OUT) -> bool:
 	if !(is_instance_valid(obj)):
-		return 0
+		return false
 	return tween_obj.interpolate_property(
 	obj,
 	property_str,
@@ -40,15 +48,20 @@ duration = period/4.0, trans_type = Tween.TRANS_LINEAR, ease_type = Tween.EASE_I
 	trans_type,
 	ease_type)
 
-func update_tween():
-	var state = self.get("tween_enum")
-	var start_pos = self.get("global_position")
-	var next_pos = start_pos
-	var offset = 2.00 * amplitude
+func update_tween() -> bool:
+	var start_pos:	Vector2 = self.get("global_position")
+	var next_pos:	Vector2 = start_pos
+	var offset:		float 	= 2.00 * self.get("amplitude")
+	var state:		int 	= self.get("tween_enum")
 	if state == 0:
 		next_pos.x = next_pos.x - offset
 		self.set("tween_enum", 1)
 	elif state == 1:
 		next_pos.x = next_pos.x + offset
 		self.set("tween_enum", 0)
-	tween_property(start_pos, next_pos, self, "global_position", tween)
+	var twn:			Tween = self.get("tween")
+	var tween_exists:	bool  = is_instance_valid(twn)
+	if tween_exists:
+		return tween_property(start_pos, next_pos, self, "global_position", twn)
+	else:
+		return false
